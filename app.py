5d9781d948978152bc6fc362d019fed987f25bcd
Empress7211 <88695190+Empress7211@users.noreply.github.com>
2024-05-07 21:55:03 +0800

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = '/InternLM_chat_1.8B_lora'
+base_path = './InternLM_chat_1.8B_lora'
 os.system(f'git clone https://code.openxlab.org.cn/Arthur_Morgan/InternLM_chat_1.8B_lora.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
